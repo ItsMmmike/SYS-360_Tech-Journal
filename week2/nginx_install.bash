417fea3911d00326b886a3b80ff88ed9d39e35c8
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2026-04-06 03:19:18 -0400

    Update nginx_install.bash

--- week2/nginx_install.bash
+++ week2/nginx_install.bash
@@ -9,7 +9,7 @@ sudo apt install vim -y
 # Download Nginx Config Files
 sudo wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/ItsMmmike/SYS-360_Tech-Journal/refs/heads/main/week2/nginx.conf"
 sudo wget -O /var/www/html/index.html "https://raw.githubusercontent.com/ItsMmmike/SYS-360_Tech-Journal/refs/heads/main/week2/index.html"
-sudo wget -O /var/www/html/cool-image.png "https://raw.githubusercontent.com/ItsMmmike/SYS-360_Tech-Journal/refs/heads/main/week2/cool-image.png"
+sudo wget -O /var/www/html/image.png "https://raw.githubusercontent.com/ItsMmmike/SYS-360_Tech-Journal/refs/heads/main/week2/image.png"
 
 # Enable and Start server
 sudo systemctl enable nginx
